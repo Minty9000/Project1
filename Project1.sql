dcef21e537e2aa836a5326491cfe613c8c8cb83f
mikelopresti21 <mikelopresti21@gmail.com>
2025-07-03 15:10:46 -0400

    Delete Project1.sql

--- Project1.sql
+++
@@ -1,9 +0,0 @@
-USE login_demo;
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(50) NOT NULL
-);
-
-INSERT INTO users (username, password) VALUES ('testuser', 'testpass');
