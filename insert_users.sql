ea7ff00d2ed952a0030ab6691b70825aaa4cb4ee
mikelopresti21 <mikelopresti21@gmail.com>
2025-07-03 15:11:00 -0400

    Delete insert_users.sql

--- insert_users.sql
+++
@@ -1,7 +0,0 @@
-USE login_demo;
-
--- Insert one or more test users
-INSERT INTO users (username, password) VALUES 
-('gilberto', 'test1'),
-('member2', 'test2'),
-('member3', 'test3');  -- add as many as needed
